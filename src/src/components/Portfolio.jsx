419066f5ae5651d51de612b8db159b8ceba80c87
Christopher-VA <cvonaltenstadt@gmail.com>
2024-03-13 13:59:09 +0800

    Adding Projects to Portfolio

--- src/src/components/Portfolio.jsx
+++ src/src/components/Portfolio.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import readmePNG from '../assets/README.png';
 import weatherMap from '../assets/New-Search.png';
 import password from "../assets/Example-of-password-created.png";
+import notes from "../assets/index-page.png";
+import quiz from "../assets/endPage.png";
+import calander from "../assets/calander.png";
 import "../css/Portfolio.css";
 
 function Portfolio() {
@@ -28,8 +31,20 @@ function Portfolio() {
         </div>
         <div>
             <h3>Notes Taker</h3>
-            <a href="https://github.com/Christopher-VA/Password-Creator">
-                <img class="card" src={password}/>
+            <a href="https://github.com/Christopher-VA/Notes-Crafter">
+                <img class="card" src={notes}/>
+            </a>
+        </div>
+        <div>
+            <h3>Quizlet</h3>
+            <a href="https://github.com/Christopher-VA/Programming-Quizlet">
+                <img class="card" src={quiz}/>
+            </a>
+        </div>
+        <div>
+            <h3>Calander</h3>
+            <a href="https://github.com/Christopher-VA/Event-Calender">
+                <img class="card" src={calander}/>
             </a>
         </div>
         </div>
